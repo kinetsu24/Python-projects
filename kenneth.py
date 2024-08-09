ef6fe3e36f0b2cb741e0bb598002ad2189ea73a0
kinetsu24 <132128049+kinetsu24@users.noreply.github.com>
2024-08-09 15:23:08 +0800

    Add files via upload

---
+++ kenneth.py
@@ -0,0 +1,6 @@
+class Customer:
+   def identify(self, name, age, gender):
+       print("I am " + name, "My age is " + age, "My gender is" + gender)
+
+cust = Customer()
+cust.identify ("Kenneth" , "19" , " Male")
